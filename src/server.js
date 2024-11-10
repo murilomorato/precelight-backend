5e0952f1bf01ea2a9fa7e7cdcc3add3752b3742a
murilomorato <murilo@nextar.com.br>
2024-11-10 14:44:17 -0300

    Add mongoose to connect to mongodb

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 const app = require('./app');
+const connectDB = require('../config/db');
+require('dotenv').config()
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
+connectDB();
 
 app.listen(PORT, () => {
     console.log(`Server up on port ${PORT}`);
