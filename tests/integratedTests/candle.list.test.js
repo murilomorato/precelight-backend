5d8af5c741c7b5a10d037af7fb3d2a9c2ee66c24
murilomorato <murilo@nextar.com.br>
2024-11-15 22:18:34 -0300

    list all candles test cases

--- tests/integratedTests/candle.list.test.js
+++ tests/integratedTests/candle.list.test.js
@@ -4,35 +4,54 @@ const app = require('../../src/app');
 const Candle = require('../../src/models/candleModel');
 
 
-describe('Candle create integrated tests', () => {
+describe('Candle list integrated tests', () => {
 
     const apiCredentials = Buffer.from(`${process.env.BASIC_AUTH_USERNAME}:${process.env.BASIC_AUTH_PASSWORD}`).toString('base64');
 
-    xit('should return all candles', async () => {
-        await Candle.create({
+    it('should return zero candles', async () => {
+
+        const response = await request(app)
+            .get('/api/candles/get-all-candles')
+            .set('Authorization', `Basic ${apiCredentials}`);
+
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual({ candles: [] });
+    });
+
+    it('should return all candles', async () => {
+        const candle01 = {
             candleType: 'simple',
             message: 'Candle 01',
             addon: [],
-            expireAt: new Date(),
+            expireAt: '2024-11-16T01:08:02.901Z',
             likes: 0,
             shares: 0,
-            uid: 'mock-uuid-01'
-        });
+            uid: 'mock-uuid-01',
+            __v: 0,//mongo data
+            _id: "6737f10961b21a3877e3d585",//mongo data
+        };
 
-        await Candle.create({
+        const candle02 = {
             candleType: 'simple',
             message: 'Candle 02',
             addon: [],
-            expireAt: new Date(),
+            expireAt: '2024-11-16T01:08:02.901Z',
             likes: 0,
             shares: 0,
-            uid: 'mock-uuid-02'
-        });
+            uid: 'mock-uuid-02',
+            __v: 0,//mongo data
+            _id: "6737f10961b21a3877e3d588",//mongo data
+        };
+
+        await Candle.create(candle01);
+        await Candle.create(candle02);
 
         const response = await request(app)
-            .get('/api/candles/get-all-candles');
+            .get('/api/candles/get-all-candles')
+            .set('Authorization', `Basic ${apiCredentials}`);
 
         expect(response.status).toBe(200);
-        expect(response.body).toEqual([{ candleType: 'simple', message: 'New Candle' }]);
+        expect(response.body).toEqual({ candles: [candle01, candle02] });
     });
+
 });
